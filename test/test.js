fe84ab4211d1a536b7e5831159cb8d28521c83fc
Norgoci Nici Nici <norgoci.nici.nicir@gmail.com>
2018-06-03 02:09:27 +0200

    introduce begin and after

--- test/test.js
+++ test/test.js
@@ -4,28 +4,43 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const eth = web3.eth;
 
-
 describe('test deployer', function() {
 
-  it('deploy, invoke contract and close', async function () {
-      this.timeout(10000);
-      const deployReport = await deployer.deploy('contract.sol');
-      assert(deployReport, 'The contract deploy report is undefined.');
-      assert(deployReport.owner, 'The contract ownwer is undefined.');
-      assert(deployReport.transactionHash, 'The contract deploy transaction hash is undefined.');
-      assert(deployReport.gas, 'The contract deploy transation gas value is undefined.');
-      assert(deployReport.contract, 'The contract metadata is undefined.');
-      assert(deployReport.contract.file, 'The contract file is undefined.');
-      assert(deployReport.contract.abi, 'The contract file is undefined.');
-      assert(deployReport.contract.address, 'The contract file is undefined.');
+  // the deploy report with all the needed information
+  // (e.g. contract abi or address).
+  // if this is undefined then the contract was not deployed
+  var deployReport;
 
+  // starts the ganache, copile and deploys the given contract.
+  // as result a deploy report is published.
+  // the report contains contract related details as its address, its owner
+  // or its abi.
+  before(async function() {
+    this.timeout(10000);
+    deployReport = await deployer.deploy('contract.sol');
+    assert(deployReport, 'The contract deploy report is undefined.');
+    assert(deployReport.owner, 'The contract ownwer is undefined.');
+    assert(deployReport.transactionHash, 'The contract deploy transaction hash is undefined.');
+    assert(deployReport.gas, 'The contract deploy transation gas value is undefined.');
+    assert(deployReport.contract, 'The contract metadata is undefined.');
+    assert(deployReport.contract.file, 'The contract file is undefined.');
+    assert(deployReport.contract.abi, 'The contract file is undefined.');
+    assert(deployReport.contract.address, 'The contract file is undefined.');
+  });
+
+  // closes the ganache server.
+  after(function() {
+    deployer.close();
+  });
+
+  // invoke a method on the cntract and prove if the result is the expected one.
+  it('invoke contract', async function () {
+      this.timeout(10000);
       const abi = deployReport.contract.abi;
       const contractAddress = deployReport.contract.address;
       const contract =  new eth.Contract(abi, contractAddress);
       const solution = await contract.methods.getSolution().call();
       assert(solution, 'The contract method can not be invoked');
       assert.equal('42', solution, 'The contract answers with wrong value.');
-
-      deployer.close();
   });
 });
