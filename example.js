c5267f85337ce50d04c6e4ef7d43a8e960c4f41a
Norgoci Nici Nici <norgoci.nici.nici@gmail.com>
2018-06-08 23:29:28 +0200

    shows the report as well

--- example.js
+++ example.js
@@ -9,5 +9,9 @@ deployer.deploy('contract.sol').then((deployReport) => {
   console.log('The contract %s is own by the user %s, the contract deploy transaction %s costs %s gas.',
   deployReport.contract.file, deployReport.owner, deployReport.transactionHash, deployReport.gas);
   console.log('The contract address is %s', deployReport.contract.address);
+
+  const report = JSON.stringify(deployReport, null, 4);
+  console.log(report);
 });
+
 //deployer.close();
